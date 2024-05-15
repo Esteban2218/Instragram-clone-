ffeb407b437fc0ef8bc683d4c59e7a7b8d5ff333
Esteban2218 <stebancatagna@gmail.com>
2024-05-15 12:03:17 -0500

    Add files via upload

---
+++ clase4ejercicio3.py
@@ -0,0 +1,24 @@
+
+print("Instagram")
+user = input("Phone number, username or email")
+password = input("Password")
+
+if user= "Esteban" and password == "12345"
+    print(f'Welcome {user}')
+    print(f'Loading your post')
+
+else:
+    print("Sorry, your password was incorrect. Please double-check your password")
+
+print("Sing up to see photos and video from your friends")
+input("Log in with Facebook")
+facebook= input("Yes or No")
+if facebook== "si":
+    print("Welcome")
+else:
+    mobile=input("Mobile number or email")
+    name=input("Full name")
+    username=input("Username")
+    password=input("password")
+
+
